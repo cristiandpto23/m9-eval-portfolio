406312f3c69f68b42ca10db19c92841fb8a9f1fd
Cristian Ahumada Vieytes <95114277+cristiandpto23@users.noreply.github.com>
2025-11-27 21:17:15 -0300

    Change database connection to use DATABASE_URL
    Updated database connection configuration to use DATABASE_URL and SSL settings.

--- models/db.js
+++ models/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
+// para conectar solo con rendercom
 const pool = new Pool({
-    host: process.env.PGHOST,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
-    port: process.env.PGPORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
